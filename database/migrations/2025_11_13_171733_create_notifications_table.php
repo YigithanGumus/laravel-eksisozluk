34801bd24ffcf0dcd9e5e2e12549924ce5e6d08f
YigithanGumus <yigithangumuss@gmail.com>
2025-11-13 20:27:47 +0300

    EksiSozluk Clone // Migrationlar oluşturuldu

---
+++ database/migrations/2025_11_13_171733_create_notifications_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id'); // kime gitti
+            $table->string('type'); // örn: favorite, follow, mention
+            $table->string('data')->nullable(); // JSON string
+            $table->boolean('is_read')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+    }
+};

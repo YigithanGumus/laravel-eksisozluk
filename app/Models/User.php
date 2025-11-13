0a7d045f3146b0a071d7789758c722eef73d8c39
YigithanGumus <yigithangumuss@gmail.com>
2025-11-13 21:00:45 +0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'username',
+        'bio',
+        'avatar',
+        'is_banned',
     ];
 
     /**
